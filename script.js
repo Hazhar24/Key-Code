b8daf8368c42f879fb3d4796b9b8e8936329bd6d
hazhar24 <hazhar24ghaderi@gmail.com>
2023-04-11 11:55:41 +0430

    Completing the project

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-﻿let $ = document
-let locationKey = $.querySelector('#location')
-let title = $.querySelector('title')
+﻿let $ = document;
+let locationKey = $.querySelector("#location");
+let title = $.querySelector("title");
 
-document.body.addEventListener('keydown', function (event) {
-	
-	starter.style.display = 'none'
-	heading.style.display = 'flex'
-	ascii.style.display = 'flex'
-	infos.style.display = 'flex'
-})
+let KeycodeElem = $.getElementById("keyCode");
+let KeyElem = $.getElementById("key");
+let locationElem = $.getElementById("location");
+let WhichElem = $.getElementById("which");
+let CodeElem = $.getElementById("code");
+
+document.body.addEventListener("keydown", function (event) {
+  event.preventDefault();
+
+  starter.style.display = "none";
+  heading.style.display = "flex";
+  ascii.style.display = "flex";
+  infos.style.display = "flex";
+
+  let eventKeyCode = event.keyCode;
+  let eventKey = event.key;
+  let eventLocation = event.location;
+  let eventWhich = event.which;
+  let eventCode = event.code;
+
+  KeycodeElem.innerHTML = eventKeyCode;
+  KeyElem.innerHTML = eventKey;
+  locationElem.innerHTML = eventLocation;
+  WhichElem.innerHTML = eventWhich;
+  CodeElem.innerHTML = eventCode;
+});
